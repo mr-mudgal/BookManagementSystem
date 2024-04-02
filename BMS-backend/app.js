b99302ee5c77369af0f6ae76a7c1d1a31196f083
mr-mudgal <100049039+mr-mudgal@users.noreply.github.com>
2024-04-03 02:03:49 +0530

    changes origin from * to the vercel link.

--- BMS-backend/app.js
+++ BMS-backend/app.js
@@ -10,7 +10,7 @@ const db = require('./DBconnection.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://book-management-system-eight.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next()
